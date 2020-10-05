241cd6006a21acf424eaf28a59ebc77769a0bd2c
justinpoblete <42792105+justinpoblete@users.noreply.github.com>
2020-10-05 01:07:23 -0700

    Add files via upload

--- schema.sql
+++ schema.sql
@@ -2,8 +2,47 @@ PRAGMA foreign_keys=ON;
 BEGIN TRANSACTION;
 DROP TABLE IF EXISTS users;
 CREATE TABLE users (
-    username VARCHAR primary key
+    username VARCHAR(20) primary key,
+    password VARCHAR(100),
+    email VARCHAR(40)
 );
 
-INSERT INTO users(username) VALUES ('johndoe');
+DROP TABLE IF EXISTS followerlist;
+CREATE TABLE followerlist (
+    username VARCHAR(20),
+    follower VARCHAR(20),
+    PRIMARY KEY (username, follower),
+    FOREIGN KEY (username) REFERENCES users(username)
+);
+/*
+DROP TABLE IF EXISTS posts;
+CREATE TABLE posts (
+    id int IDENTITY(1,1) NOT NULL primary key,
+    text VARCHAR(200),
+    TIMESTAMP DATE,
+    author VARCHAR(20),
+    FOREIGN KEY (author) REFERENCES users(username)
+);
+
+*/
+
+INSERT INTO users(username,password,email) VALUES ('apatriche0','zenBdE','grutigliano0@uiuc.edu');
+INSERT INTO users(username,password,email) VALUES ('naleso1','9BLyYSgNclK','cshildrick1@bloomberg.com');
+INSERT INTO users(username,password,email) VALUES ('ghobden2','JoV7tSiK','jmaund2@guardian.co.uk');
+INSERT INTO users(username,password,email) VALUES ('cprodrick3','OKkRvbX','dhann3@barnesandnoble.com');
+INSERT INTO users(username,password,email) VALUES ('cferrarone4','DptrjL6UCh','afinlater4@cisco.com');
+
+INSERT INTO followerlist(username,follower) VALUES ('apatriche0','apatriche0');
+INSERT INTO followerlist(username,follower) VALUES ('naleso1','naleso1');
+INSERT INTO followerlist(username,follower) VALUES ('ghobden2','ghobden2');
+INSERT INTO followerlist(username,follower) VALUES ('cprodrick3','cprodrick3');
+INSERT INTO followerlist(username,follower) VALUES ('cferrarone4','cferrarone4');
+
+INSERT INTO followerlist(username,follower) VALUES ('naleso1','apatriche0');
+INSERT INTO followerlist(username,follower) VALUES ('cferrarone4','cprodrick3');
+INSERT INTO followerlist(username,follower) VALUES ('naleso1','ghobden2');
+INSERT INTO followerlist(username,follower) VALUES ('ghobden2','cferrarone4');
+INSERT INTO followerlist(username,follower) VALUES ('cferrarone4','apatriche0');
+
+
 COMMIT;
--- users.py
+++ users.py
@@ -1,7 +1,7 @@
 import flask
 from flask import request, jsonify, g
 import sqlite3
-
+from werkzeug.security import generate_password_hash,check_password_hash
 
 app = flask.Flask(__name__)
 app.config.from_envvar('APP_CONFIG')
@@ -55,9 +55,56 @@ def users_all():
     return jsonify(all_users)
 
 @app.route('/users', methods=['POST'])
-def create(username):
-    createUser = query_db('INSERT INTO users(username) VALUES ("%s")', (username))
-    return createUser
+def createUser():
+    query_params = request.get_json()
+    username = query_params['username']
+    password = query_params['password']
+    password = generate_password_hash(password)
+    email = query_params['email']
+    db = get_db()
+    
+    query_db('INSERT INTO users(username,password,email) VALUES (?,?,?);',(username,password,email))
+    db.commit()
+    return "created",201
+    
+   
+
+@app.route('/users/<username>', methods=['PUT'])
+def addFollower(username):
+    query_params = request.get_json()
+    original = query_params['username']
+    db = get_db()
+    
+    query_db('INSERT INTO followerlist(username,follower) VALUES (?,?);',(original,username))
+    db.commit()
+    return "Follower added"
+
+@app.route('/users/<username>', methods=['DELETE'])
+def removeFollower(username):
+    query_params = request.get_json()
+    original = query_params['username']
+    db = get_db()
+    
+    query_db('DELETE FROM followerlist WHERE username = ? AND follower = ?',(original,username))
+    db.commit()
+    return "Follower deleted"
+
+@app.route('/users/<username>/<password>', methods=['GET'])
+def authenticateUser(username,password):
+    
+
+    auth = query_db('SELECT * FROM users WHERE username = ?;',(username,))
+    stored = auth[0]['password']
+    result = check_password_hash(stored,password)
+    if result == True:
+        return "Authorized"
+        
+    else:
+        message = {'message': "Authenticate."}
+        resp = jsonify(message)
+
+        resp.status_code = 401
+        return resp
 
 
 @app.errorhandler(404)
